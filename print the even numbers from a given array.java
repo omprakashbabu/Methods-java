a56d285d99f8f0920de670f553988558995c769b
omprakashbabu <116138027+omprakashbabu@users.noreply.github.com>
2023-10-15 01:55:20 +0530

    Update print the even numbers from a given array.java

--- print the even numbers from a given array.java
+++ print the even numbers from a given array.java
@@ -1,3 +1,7 @@
+
+//Write a Java program (using function) to print the even numbers from a given list.
+//(Note : function type - with arguments but no return values)
+
 import java.util.Scanner;
 
 public class Main
@@ -19,11 +23,24 @@ public class Main
 	
 	public static void even_num(int arr[], int n){
 	    for(int i=0;i<n;i++){
-            if(arr[i] % 2 == 0){
-                System.out.println(arr[i]);
-            }
-		}
-	}
-	    
-	    
+            	if(arr[i] % 2 == 0){
+                    System.out.println(arr[i]);
+            	}
+	    }
+	} 	    
 }
+
+/*
+Output
+Enter the number of elements to be stored : 5
+Enter 5 integers : 
+1
+2
+4
+3
+6
+The even numbers are 
+2
+4
+6
+*/
